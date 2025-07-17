81c7efa456ae661332a3db0e49129a3d2ea87e40
Mati Costamagna <matias.costamagna@mi.unc.edu.ar>
2025-07-17 11:45:52 -0300

    Agregado tiempo de ejecucion en el Logger

--- src/Main.java
+++ src/Main.java
@@ -35,11 +35,11 @@ public class Main {
         };
 
         // Inicialización de la Red de Petri, la política y el monitor
-        //long[] alpha = {0,1,0,1,1,0,1,0,1,1,1,0};
-        long[] alpha = {0,10,0,10,10,0,10,0,10,10,10,0};
+        long[] alpha = {0,0,0,0,0,0,0,0,0,0,0,0};
+        //long[] alpha = {0,10,0,10,10,0,10,0,10,10,10,0};
         long[] beta = {Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE,Long.MAX_VALUE};
          RdP red = new RdP(matrizI,marcadoInicial, alpha, beta);
-        PoliticaInterface politica = new PoliticaPrioritaria(); // o PoliticaPrioritaria()
+        PoliticaInterface politica = new PoliticaPrioritaria(); // PoliticaAleatoria() o PoliticaPrioritaria()
         Monitor monitor = new Monitor(red, politica);
 
         // Inicialización del Logger y su hilo
