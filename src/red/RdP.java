17c804f40ac85b952b4b7736fe7ce98d902c922d
Mati Costamagna <matias.costamagna@mi.unc.edu.ar>
2025-08-30 12:41:52 -0300

    Merging

--- src/red/RdP.java
+++ src/red/RdP.java
@@ -47,7 +47,9 @@ public class RdP {
         } else if ((marcado[4] + marcado[5] + marcado[6] + marcado[7] + marcado[8] + marcado[9] + marcado[10]) != 1){
             return false;
         }
-        return true;
+        return (marcado[0] + marcado[1] + marcado[3] + marcado[4] + marcado[5] + marcado[7] + marcado[8] + marcado[9] + marcado[10] + marcado[11]) == 3 &&
+                ((marcado[1] + marcado[2]) == 1) &&
+                ((marcado[4] + marcado[5] + marcado[6] + marcado[7] + marcado[8] + marcado[9] + marcado[10]) == 1);
     }
 
     private boolean testVentanaTiempo(int transition) {
